b7a1080334f89fff0176d244f54d7cca3e9a86b9
leongrubisic <lxg6892@gmail.com>
2020-07-09 16:53:12 +0200

    Update login.component.ts

--- employee-time-tracker/src/app/login/login.component.ts
+++ employee-time-tracker/src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { LoginService } from '../shared/login.service';
-
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +10,7 @@ import { LoginService } from '../shared/login.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(public loginService: LoginService) { }
+  constructor(public loginService: LoginService, private toastr: ToastrService) { }
 
   ngOnInit() {
     this.resetForm();
@@ -19,6 +19,7 @@ export class LoginComponent implements OnInit {
   onSubmit(form: NgForm) {
     if (this.loginService.loginFormData.username === "" || this.loginService.loginFormData.password === "") {
       //toastr here and then reset form
+      this.toastr.error('Please fill out both fields to log in.', 'Error');
     } else {
       //check role and redirect to page
     }
