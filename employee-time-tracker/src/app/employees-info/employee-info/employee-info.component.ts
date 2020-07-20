c00a40830ed9ea4ec66fd2fc7ad91f6588a99c11
Ivan Marusic <imarusic@hrcloud.corp>
2020-07-20 11:39:44 +0200

    reset buttons

--- employee-time-tracker/src/app/employees-info/employee-info/employee-info.component.ts
+++ employee-time-tracker/src/app/employees-info/employee-info/employee-info.component.ts
@@ -8,7 +8,7 @@ import { ToastrService } from 'ngx-toastr';
   selector: 'app-employee-info',
   templateUrl: './employee-info.component.html',
   styleUrls: ['./employee-info.component.css']
-})
+}) 
 export class EmployeeInfoComponent implements OnInit {
 
   constructor(private service: EmployeeInfoService, private toastr: ToastrService) { }
