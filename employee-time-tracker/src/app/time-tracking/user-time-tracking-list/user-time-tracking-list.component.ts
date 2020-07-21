fa13cd03dcc36b826bbad6d66f72afb384a3a43d
Ivan Marusic <imarusic@hrcloud.corp>
2020-07-21 13:14:01 +0200

    created timecounter for user hours worked

--- employee-time-tracker/src/app/time-tracking/user-time-tracking-list/user-time-tracking-list.component.ts
+++ employee-time-tracker/src/app/time-tracking/user-time-tracking-list/user-time-tracking-list.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { TimeTrackService } from '../../services/time-track.service';
 import { ToastrService } from 'ngx-toastr';
 import { AuthenticationService } from 'src/app/services/authentication.service';
-import { EmployeeInfo } from 'src/app/models/employee-info.model';
 import { TimeTrack } from 'src/app/models/time-track.model';
 
 @Component({
@@ -12,15 +11,15 @@ import { TimeTrack } from 'src/app/models/time-track.model';
 })
 export class UserTimeTrackingListComponent implements OnInit {
 
-  public currentUserTimeStamps: TimeTrack[];
+  public currentUserTimeStamps: TimeTrack[] = [];
+  public totalTimeWorked: number;
 
   constructor(public timeService: TimeTrackService, private toastr: ToastrService, private authenticationService: AuthenticationService) { 
 
   }
 
   ngOnInit(): void {
-    //this.timeService.refreshTimeList();
-    this.getCurrentUserTimeTrack()
+    this.getCurrentUserTimeTrack();
   }
 
   getCurrentUserTimeTrack(): void{
@@ -29,11 +28,39 @@ export class UserTimeTrackingListComponent implements OnInit {
     allTimeTracks.forEach(function (value){
       for(let i = 0; i < value.length; i++){
         if(value[i].employee_init_id === this.authenticationService.currentUserValue.id_num){
-          this.currentUserTimeStamps.add(value[i]);
+          this.currentUserTimeStamps.push(value[i]);
         }
       }
-    });
+      this.totalTimeWorked = this.calculateTotalTimeWorked();
+      //console.log(this.totalTimeWorked+"finally");
+    }.bind(this));
+  }
 
+  calculateTotalTimeWorked(): number{
+    let totalHoursWorked = 0;
+    this.currentUserTimeStamps.forEach(date => {
+      totalHoursWorked += this.calculateTimeWorkedOfTheDay(date.date_of_work, date.time_in, date.time_out);
+      //totalHoursWorked += this.calculateTimeWorkedOfTheDay("2020-07-21T00:00:00","11:55:55","17:35:57");
+      console.log(totalHoursWorked);
+    });
+    return totalHoursWorked
+  }
+
+  calculateTimeWorkedOfTheDay(date: String, _start: String, _end: String){
+    let d1 = new Date(date.substring(0,10) +"T"+_start+"Z");  
+    let d2 = new Date(date.substring(0,10) +"T"+_end+"Z"); 
+    if(d1.getTime() >= d2.getTime()){
+      return 0;
+    }
+    let total = d2.getTime() - d1.getTime();
+    console.log(this.convertMilisecondsToHours(total).toFixed(3));
+    return parseFloat(this.convertMilisecondsToHours(total).toFixed(3));
+
+  }
+
+  convertMilisecondsToHours(ms: number):number{
+      let hours =((ms/1000)/60)/60;
+      return hours;
   }
   
 }
