3c0c1cba98def62c0aa2191ca5d4831f8c165b6f
leongrubisic <lxg6892@gmail.com>
2020-07-27 13:43:55 +0200

    Gitignore update

---
+++ EmployeeTimeTracker/EmployeeTimeTracker/Models/EmployeeManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeTimeTracker.Models
+{
+    public partial class EmployeeManager
+    {
+        public int manager_id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public int employee_id { get; set; }
+    }
+}
